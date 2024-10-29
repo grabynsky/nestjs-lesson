274df7f80cc995541555e6bfd3c8ca5a1334d6c2
Grabynsky Andrew <hrabynsky@gmail.com>
2024-10-29 20:34:17 +0200

    create article

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ArticlesModule],
   controllers: [],
   providers: [],
 })
